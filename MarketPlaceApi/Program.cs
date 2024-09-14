e2523b62b4cfec2782cdee6a71a2c76d906d1d3a
ZFidelis <144962808+ZFidelis@users.noreply.github.com>
2024-09-14 17:05:51 -0300

    create Banco.cs and created Gets

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -1,6 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "X Marketplace!");
+
+#region Cliente
+app.MapGet("/clientes", async (AppDbContext db) =>
+    await db.Clientes.ToListAsync());
+
+app.MapGet("/clientes/{id}", async (int id, AppDbContext db) => 
+    await db.Clientes.FindAsync(id)
+      is Cliente cliente
+        ? Results.Ok(cliente)
+          : Results.NotFound());
+
+#endregion
+#region Vendedor
+app.MapGet("/vendedores", async (AppDbContext db) =>
+    await db.Vendedores.ToListAsync());
+
+app.MapGet("/vendedores/{id}", async (int id, AppDbContext db) => 
+    await db.Vendedores.FindAsync(id)
+      is Vendedor vendedor
+        ? Results.Ok(vendedor)
+          : Results.NotFound());
+
+#endregion
+#region Administrador
+app.MapGet("/administradores", async (AppDbContext db) =>
+    await db.Administradores.ToListAsync());
+
+app.MapGet("/administradores/{id}", async (int id, AppDbContext db) => 
+    await db.Administradores.FindAsync(id)
+      is Administrador administrador
+        ? Results.Ok(administrador)
+          : Results.NotFound());
+
+#endregion
 
 app.Run();
