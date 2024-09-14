5d3930b58dd132dc953b05ff752628d8de088f5d
JoaoPauloClass <theofficialspam8@gmail.com>
2024-09-14 19:14:37 -0300

    Metodo get

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -8,13 +8,27 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/vendas", async (AppDbContext db) =>
-    await db.Vendas.ToListAsync());
+{
+    var vendas = await db.Vendas
+        .Include(v => v.ProdutosVendidos)
+        .ToListAsync();
 
-app.MapGet("/vendas/{id}", async (int id, AppDbContext db) => 
-    await db.Vendas.FindAsync(id)
-      is Venda venda
-        ? Results.Ok(venda)
-          : Results.NotFound());
+    return Results.Ok(vendas);
+});
+
+app.MapGet("/vendas/{id}", async (int id, AppDbContext db) =>
+{
+    var venda = await db.Vendas
+        .Include(v => v.ProdutosVendidos)
+        .FirstOrDefaultAsync(v => v.Id == id);
+
+    if (venda == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(venda);
+});
 
 app.MapPost("/vendas", async (Venda venda, AppDbContext db) => {
     db.Vendas.Add(venda);
