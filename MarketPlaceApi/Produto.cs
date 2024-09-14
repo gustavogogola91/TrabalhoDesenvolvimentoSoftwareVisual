5647b4753042d5e68eac618e13d39ea7d336547b
gustavogogola91 <gustavogogola3@gmail.com>
2024-09-14 16:56:31 -0300

    Classe Produto criada

---
+++ MarketPlaceApi/Produto.cs
@@ -0,0 +1,10 @@
+public class Produto
+{
+
+    private String nome { get; set; }
+    private String? descricao { get; set; }
+    private int quantidade {get; set;}
+    private long valor { get; set; }
+    private Vendedor vendedor {get; set;}
+
+}
