183c28e69505ccdd22ced39ac0074bcebce81403
gustavogogola91 <gustavogogola3@gmail.com>
2024-09-14 17:28:20 -0300

    Adicionados metodos Get para a classe Produto

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -7,3 +7,11 @@ builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
+
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+
+app.MapGet("/produtos/disponiveis", async (AppDbContext db) => await db.Produtos.Where(p => p.Quantidade >= 1).ToListAsync());
+
+app.MapGet("/produtos/tag/{tag}", async (string tag, AppDbContext db) => await db.Produtos.Where(p => p.Tag == tag).ToListAsync());
+
+app.MapGet("/produto/{id}", async (int id, AppDbContext db) => await db.Produtos.FindAsync(id) is Produto produto ? Results.Ok(produto) : Results.NotFound());
