8cf89c74f86308b7f6d7213b39fd686f2ec16705
ZFidelis <isaaczapo@hotmail.com>
2024-09-14 18:58:29 -0300

    Put and Delete working

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -14,6 +14,7 @@ var app = builder.Build();
 app.MapGet("/", () => "X Marketplace!");
 
 #region GetCliente
+
 app.MapGet("/clientes", async (AppDbContext db) =>
     await db.Clientes.ToListAsync());
 
@@ -26,10 +27,47 @@ app.MapGet("/clientes/{id}", async (int id, AppDbContext db) =>
 #endregion
 
 #region PostCliente
+
 app.MapPost("/clientes", async (Cliente cliente, AppDbContext db) => {
     db.Clientes.Add(cliente);
     await db.SaveChangesAsync();
     return Results.Created($"/clientes/{cliente.Id}", cliente);
+});
+
+#endregion
+
+#region PutCliente
+
+app.MapPut("clientes/{id}", async (int id, Cliente clienteAlterado, AppDbContext db) =>
+{
+    var cliente = await db.Clientes.FindAsync(id);
+    if (cliente is null) return Results.NotFound();
+
+    cliente.Nome = clienteAlterado.Nome;
+    cliente.Email = clienteAlterado.Email;
+    cliente.Senha = clienteAlterado.Senha;
+    cliente.IdCarrinho = clienteAlterado.IdCarrinho;
+    cliente.IdComprasHist = clienteAlterado.IdComprasHist;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+#endregion
+
+#region DeleteCliente
+
+app.MapDelete("clientes/{id}", async (int id, AppDbContext db) =>
+{
+    if(await db.Clientes.FindAsync(id) is Cliente cliente){
+
+        db.Clientes.Remove(cliente);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    return Results.NotFound();
+ 
 });
 
 #endregion
