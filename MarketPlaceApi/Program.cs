f1044eb3f2114f77c8cdec7a2a2a3ac089bcdf81
gustavogogola91 <gustavogogola3@gmail.com>
2024-09-14 17:39:51 -0300

    app.Run added

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -15,3 +15,5 @@ app.MapGet("/produtos/disponiveis", async (AppDbContext db) => await db.Produtos
 app.MapGet("/produtos/tag/{tag}", async (string tag, AppDbContext db) => await db.Produtos.Where(p => p.Tag == tag).ToListAsync());
 
 app.MapGet("/produto/{id}", async (int id, AppDbContext db) => await db.Produtos.FindAsync(id) is Produto produto ? Results.Ok(produto) : Results.NotFound());
+
+app.Run();
