e18f3d90343eaabd73279e599e0756c08aedb448
gustavogogola91 <gustavogogola3@gmail.com>
2024-09-17 21:46:25 -0300

    Criado arquico Produto API para controle das requisições

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Marketplace!");
+app.MapProdutosAPI();
 
 #region Cliente
 #region GetCliente
@@ -325,58 +326,6 @@ app.MapDelete("administradores/{id}", async (int id, AppDbContext db) =>
 
 #endregion
 #endregion
-
-
-
-
-app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
-
-app.MapGet("/produtos/disponiveis", async (AppDbContext db) => await db.Produtos.Where(p => p.Quantidade >= 1).ToListAsync());
-
-app.MapGet("/produtos/tag/{tag}", async (string tag, AppDbContext db) => await db.Produtos.Where(p => p.Tag == tag).ToListAsync());
-
-app.MapGet("/produto/{id}", async (int id, AppDbContext db) => await db.Produtos.FindAsync(id) is Produto produto ? Results.Ok(produto) : Results.NotFound());
-
-app.MapPost("/produto", async (Produto produto, AppDbContext db) =>
-{
-    db.Produtos.Add(produto);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/tarefas/{produto.Id}", produto);
-});
-
-app.MapPut("/produto/{id}", async (int id, Produto produtoAlterado, AppDbContext db) =>
-{
-
-    var produto = await db.Produtos.FindAsync(id);
-    if (produto is null) return Results.NotFound();
-
-    produto.Nome = produtoAlterado.Nome;
-    produto.Descricao = produtoAlterado.Descricao;
-    produto.Quantidade = produtoAlterado.Quantidade;
-    produto.Valor = produtoAlterado.Valor;
-    produto.IdVendedor = produtoAlterado.IdVendedor;
-    produto.Tag = produtoAlterado.Tag;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-
-});
-
-app.MapDelete("/produto/{id}", async (int id, AppDbContext db) =>
-{
-
-    if (await db.Produtos.FindAsync(id) is Produto produto)
-    {
-
-        db.Produtos.Remove(produto);
-
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-    return Results.NotFound();
-});
 
 app.MapGet("/carrinho", async (AppDbContext db) =>
     await db.Carrinhos
