59d7c82daa3a9674e52c6d1e75c189ea2b80c475
JoaoPauloClass <theofficialspam8@gmail.com>
2024-09-19 15:06:21 -0300

    Model VendaAPI creation

--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -10,6 +10,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Marketplace!");
 app.MapProdutosAPI();
+app.MapVendasAPI();
 
 #region Cliente
 #region GetCliente
@@ -169,61 +170,7 @@ app.MapDelete("/enderecos/{id}", async (int id, AppDbContext db) =>
     return Results.NotFound();
 });
 
-app.MapGet("/vendas", async (AppDbContext db) =>
-{
-    var vendas = await db.Vendas
-        .Include(v => v.ProdutosVendidos)
-        .ToListAsync();
 
-    return Results.Ok(vendas);
-});
-
-app.MapGet("/vendas/{id}", async (int id, AppDbContext db) =>
-{
-    var venda = await db.Vendas
-        .Include(v => v.ProdutosVendidos)
-        .FirstOrDefaultAsync(v => v.Id == id);
-
-    if (venda == null)
-    {
-        return Results.NotFound();
-    }
-
-    return Results.Ok(venda);
-});
-
-app.MapPost("/vendas", async (Venda venda, AppDbContext db) => {
-    db.Vendas.Add(venda);
-    await db.SaveChangesAsync();
-    return Results.Created($"/vendas/{venda.Id}", venda);
-});
-
-app.MapPut("vendas/{id}", async (int id, Venda vendaAlterada, AppDbContext db) =>
-{
-    var venda = await db.Vendas.FindAsync(id);
-    if (venda is null) return Results.NotFound();
-
-    venda.IdCliente = vendaAlterada.IdCliente;
-    venda.IdVendedor = vendaAlterada.IdVendedor;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-
-app.MapDelete("vendas/{id}", async (int id, AppDbContext db) =>
-{
-    if(await db.Vendas.FindAsync(id) is Venda venda){
-
-        db.Vendas.Remove(venda);
-
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-    return Results.NotFound();
- 
-});
 
 app.MapGet("/cupom", async (AppDbContext db) =>
     await db.Cupons.ToListAsync());
