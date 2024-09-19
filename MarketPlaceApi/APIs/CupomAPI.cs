81d032955f7a9f00994b51ca33776ed0d39ccd9e
karasawa <144966992+karasawaa@users.noreply.github.com>
2024-09-19 20:31:38 +0000

    changes

---
+++ MarketPlaceApi/APIs/CupomAPI.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+
+public static class CupomAPI
+{
+    public static void MapCuponsAPI(this WebApplication app)
+    {
+
+        var group = app.MapGroup("/cupons");
+
+        group.MapGet("/", async (AppDbContext db) =>
+        {
+            return await db.Cupons.ToListAsync();
+        });
+
+        group.MapGet("/{id}", async (int id, AppDbContext db) => 
+        {
+            return await db.Cupons.FindAsync(id) is Cupom cupom ? Results.Ok(cupom) : Results.NotFound();
+        });
+    
+        group.MapPost("/", async (Cupom cupom, AppDbContext db) => 
+        {
+            db.Cupons.Add(cupom);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/cupom/{cupom.Id}", cupom);
+        });
+
+        group.MapPut("/{id}", async (int id, Cupom cupomAlterado, AppDbContext db) =>
+        {
+            var cupom = await db.Cupons.FindAsync(id);
+            if (cupom is null) return Results.NotFound();
+        
+            cupom.Codigo = cupomAlterado.Codigo;
+            cupom.Desconto = cupomAlterado.Desconto;
+            cupom.Usado = cupomAlterado.Usado;
+            cupom.ValorMin = cupomAlterado.ValorMin;
+        
+            await db.SaveChangesAsync();
+        
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", async (int id, AppDbContext db) =>
+        {
+            if (await db.Cupons.FindAsync(id) is Cupom cupom)
+            {
+            
+                db.Cupons.Remove(cupom);
+
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+            }
+            return Results.NotFound();
+        });
+    }
+}
--- MarketPlaceApi/Program.cs
+++ MarketPlaceApi/Program.cs
@@ -10,6 +10,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Marketplace!");
 app.MapProdutosAPI();
+app.MapCuponsAPI();
 
 #region Cliente
 #region GetCliente
@@ -223,51 +224,6 @@ app.MapDelete("vendas/{id}", async (int id, AppDbContext db) =>
     }
     return Results.NotFound();
  
-});
-
-app.MapGet("/cupom", async (AppDbContext db) =>
-    await db.Cupons.ToListAsync());
-
-app.MapGet("/cupom/{id}", async (int id, AppDbContext db) => 
-    await db.Cupons.FindAsync(id)
-      is Cupom cupom
-        ? Results.Ok(cupom)
-          : Results.NotFound());
-    
-app.MapPost("/cupom", async (Cupom cupom, AppDbContext db) => {
-    db.Cupons.Add(cupom);
-    await db.SaveChangesAsync();
-    return Results.Created($"/cupom/{cupom.Id}", cupom);
-});
-
-app.MapPut("cupom/{id}", async (int id, Cupom cupomAlterado, AppDbContext db) =>
-{
-    var cupom = await db.Cupons.FindAsync(id);
-    if (cupom is null) return Results.NotFound();
-
-    cupom.Codigo = cupomAlterado.Codigo;
-    cupom.Desconto = cupomAlterado.Desconto;
-    cupom.Usado = cupomAlterado.Usado;
-    cupom.ValorMin = cupomAlterado.ValorMin;
-  
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-
-});
-
-app.MapDelete("cupom/{id}", async (int id, AppDbContext db) =>
-{
-    if(await db.Cupons.FindAsync(id) is Cupom cupom){
-
-        db.Cupons.Remove(cupom);
-        
-
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-    return Results.NotFound();
-
 });
 
 #region Administrador
