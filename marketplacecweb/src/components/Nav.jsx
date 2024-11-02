8f529e2fc9cb4724eab925406f76dbdf18d9b1b3
gustavogogola91 <gustavogogola3@gmail.com>
2024-11-02 20:40:03 -0300

    Nav adicionado

--- marketplacecweb/src/components/Nav.jsx
+++ marketplacecweb/src/components/Nav.jsx
@@ -1,20 +1,20 @@
 import { NavLink } from "react-router-dom";
 
-function NavApp() {
+function Nav() {
   return (
-    <nav className="">
-      <ul>
-        <li>
+    <nav className="w-full bg-purple text-cream">
+      <ul className="w-full flex flex-row gap-4 h-20 justify-center pt-8 ">
+        <li className="text-base">
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
+        <li className="text-base">
           <NavLink to="/produtos">Produtos</NavLink>
         </li>
-        <li>
+        <li className="text-base">
           <NavLink to="/usuario">Usuario</NavLink>
         </li>
       </ul>
     </nav>
   );
 }
-export default NavApp;
+export default Nav;
