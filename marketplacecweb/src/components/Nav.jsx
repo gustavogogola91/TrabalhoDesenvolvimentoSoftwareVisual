59d8ffe06973fe7a5b0e8405690aa2e7a67cf2dc
JoaoPauloClass <theofficialspam8@gmail.com>
2024-11-02 19:09:52 -0300

    Add Header/Footer

--- marketplacecweb/src/components/Nav.jsx
+++ marketplacecweb/src/components/Nav.jsx
@@ -1,17 +1,18 @@
 import { NavLink } from "react-router-dom";
+import userImg from "../imgs/icon-user.png"
 
 function NavApp() {
   return (
-    <nav className="">
-      <ul>
-        <li>
+    <nav>
+      <ul className="flex flex-row gap-5">
+        <li className="flex flex-row items-center bg-purple px-3 py-2 rounded-3xl hover:bg-very-light-purple hover:text-purple transition duration-300 ease-in-out">
           <NavLink to="/">Home</NavLink>
         </li>
-        <li>
+        <li className="flex flex-row items-center bg-purple px-3 py-2 rounded-3xl hover:bg-very-light-purple hover:text-purple transition duration-300 ease-in-out">
           <NavLink to="/produtos">Produtos</NavLink>
         </li>
-        <li>
-          <NavLink to="/usuario">Usuario</NavLink>
+        <li className="flex flex-row items-center bg-purple px-3 py-2 rounded-3xl hover:bg-very-light-purple hover:text-purple transition duration-300 ease-in-out">
+          <NavLink to="/usuario"><img src={ userImg } alt="" className="w-[35px]"/></NavLink>
         </li>
       </ul>
     </nav>
