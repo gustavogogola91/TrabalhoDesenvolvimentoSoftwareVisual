7ad5fbafa48f0c7ea3dd43dc01bd2c574b724599
gustavogogola91 <gustavogogola3@gmail.com>
2024-11-02 20:37:16 -0300

    nova configuração tailwind

--- marketplacecweb/tailwind.config.js
+++ marketplacecweb/tailwind.config.js
@@ -1,19 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        "purple": "#3b234a",
+        purple: "#3b234a",
         "light-purple": "#523961",
         "very-light-purple": "#523961",
-        "gray": "#c3bbc9",
-        "cream": "#d4c7bf",
+        gray: "#c3bbc9",
+        cream: "#d4c7bf",
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-spinner": {
+          "-moz-appearance": "textfield",
+        },
+        ".no-spinner::-webkit-inner-spin-button": {
+          "-webkit-appearance": "none",
+          margin: "0",
+        },
+        ".no-spinner::-webkit-outer-spin-button": {
+          "-webkit-appearance": "none",
+          margin: "0",
+        },
+      });
+    },
+  ],
+};
