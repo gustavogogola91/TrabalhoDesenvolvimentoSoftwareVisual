4bd7d669de8c32fbd4a953f70ab78d00cad19f95
JoaoPauloClass <theofficialspam8@gmail.com>
2024-11-02 16:15:56 -0300

    first commit

---
+++ marketplacecweb/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../imgs/logo.png"
+
+function Header() {
+    
+
+    return(
+        <header>
+            <img src={ logo } alt="Logo Marketplace" />
+        </header>
+
+    )
+}
