13fd1a2fbfecf8357a2f61e9f8ef8f3157255685
ZFidelis <isaaczapo@hotmail.com>
2024-11-02 19:58:00 -0300

    Main files to IsaacWeb branch

--- marketplacecweb/src/App.jsx
+++ marketplacecweb/src/App.jsx
@@ -1,20 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
-import Login from  "./pages/Login";
-
-
-
-
-import NavApp from "./components/Nav";
+import Layout from "./components/Layout";
+import Login from "./pages/Login"
+import LayoutLogin from "./components/LayoutLogin";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<NavApp />} />
-        <Route path="/produtos" element={<NavApp />} />
-        <Route path="/usuario" element={<NavApp />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={ <Layout>  </Layout> } />
+        <Route path="/produtos" element={<Layout>  </Layout>} />
+        <Route path="/usuario" element={<Layout>  </Layout>} />
+        <Route path="/login" element={<LayoutLogin> <Login/> </LayoutLogin>} />
       </Routes>
     </>
   );
