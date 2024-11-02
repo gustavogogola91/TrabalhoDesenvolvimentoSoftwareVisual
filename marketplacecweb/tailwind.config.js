641283908c024acf3eaafa4505cea2a4b1912c2d
JoaoPauloClass <theofficialspam8@gmail.com>
2024-11-02 15:48:38 -0300

    color pallete

--- marketplacecweb/tailwind.config.js
+++ marketplacecweb/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple": "#3b234a",
+        "light-purple": "#523961",
+        "very-light-purple": "#523961",
+        "gray": "#c3bbc9",
+        "cream": "#d4c7bf",
+      },
+    },
   },
   plugins: [],
 }
